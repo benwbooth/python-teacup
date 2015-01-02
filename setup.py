9f3777a1b2885a0715eea84ac726cc8dbcb66f4b
Ben Booth <benwbooth@gmail.com>
2015-01-02 13:44:08 -0800

    remove redundant documentation

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
-"""
-HTML Templating DSL for Python.
+import setuptools, os
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as doc:
+    __doc__=doc.read()
 
-A simple HTML templating library that uses a DSL
-mini-language. I tried to design it to be similar in feel
-to Teacup/CoffeeKup (https://github.com/goodeggs/teacup). I
-also took inspiration from jamescasbon's template.py script
-(https://gist.github.com/jamescasbon/1461441).
-
-Also includes the md() function for embedding markdown, and an option
-for outputting prettified HTML.
-
-Example usage::
-
-    from teacup import *
-
-    with html:
-        with head:
-            pass
-        with body:
-            with p:
-                md("This is a **test!**")
-            with p({"class":"test"}, id="testme"):
-                text("Another test")
-                a("Click here", href="https://google.com")
-
-    print(render())
-
-The python "with" keyword is used to nest HTML tags.
-
-"""
-
-from setuptools import setup
-
-setup(
+setuptools.setup(
     name='teacup',
-    version='0.8',
+    version='0.9',
     url='https://github.com/benwbooth/python-teacup',
     author='Ben Booth',
     author_email='benwbooth@gmail.com',
